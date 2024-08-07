2a3555b64d3e08b171176309452b9ae442cc5f29
Ahmed Shaker <amhmeed31@gmail.com>
2024-08-07 14:52:23 +0300

    add some any

--- lib/view/screens/MainScreen.dart
+++ lib/view/screens/MainScreen.dart
@@ -51,149 +51,333 @@ class _MainScreenState extends State<MainScreen> {
       backgroundColor: ColorUsed.PrimaryBackground,
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 2.0,
-        toolbarHeight: 10.h ,
-        title: Text(
-          S.of(context).Select_Service,
-          style: TextStyle(
-            color: AppTheme.white,
-            fontSize: 20.sp,
-            fontWeight: FontWeight.bold,
-          ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
         ),
-        centerTitle: true,
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20),
-            ),
-            gradient: LinearGradient(
-              colors: [ColorUsed.primary, ColorUsed.second],
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-            ),
+        leading: IconButton(
+          icon: Icon(
+           
+            Icons.menu,
+            color: Colors.white,
           ),
+          onPressed: () {
+            _scaffoldKey.currentState?.openDrawer();
+          },
         ),
-        systemOverlayStyle: SystemUiOverlayStyle.light,
-      ),
-      //! body
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 3.0,
-                  color: ColorUsed.primary,
-                ),
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(20),
-                ),
-              ),
-              transformAlignment: Alignment.center,
-              margin: const EdgeInsets.only(
-                top: 80.0,
-                bottom: 120.0,
-                left: 16.0,
-                right: 16.0,
-              ),
-              padding: const EdgeInsets.all(8.0),
-              alignment: Alignment.center,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          S.of(context).Select_Service,
-                          style: TextStyle(
-                            color: ColorUsed.primary,
-                            fontSize: 18.sp,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
+        backgroundColor: ColorUsed.primary,
+        elevation: 5,
+        toolbarHeight: getheight(10),
+        actions: [
+          IconButton(
+             key: lang,
+            icon: Icon(
+              Icons.language,
+              color: AppTheme.notWhite,
+              semanticLabel: S.current.line,
+            ),
+            onPressed: () async {
+              final read = context.read<LanguageController>();
+              await showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text(S.of(context).chanage_lang),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        if (shared!.getString('lang') == 'en') {
+                          Navigator.of(context).pop();
+                          return;
+                        }
+                        read.changeLanguage('en');
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('English'),
                     ),
-                    const SizedBox(
-                      height: 50.0,
-                    ),
-                    Row(
-                      children: [
-                        ButtonSelect(
-                          title: S.of(context).blood_type,
-                          onPressed: () {
-                            context
-                                .read<Providers>()
-                                .managerScreen(BloodScreen.ROUTE, context);
-                          },
-                        ),
-                        const SizedBox(
-                          width: 5.0,
-                        ),
-                        ButtonSelect(
-                          title: S.of(context).doctor,
-                          onPressed: () {
-                            context.read<Providers>().managerScreen(
-                                  DoctorScreen.ROUTE,
-                                  context,
-                                );
-                          },
-                        ),
-                      ],
-                    ), // runs after the above w/new duration
-
-                    const SizedBox(
-                      height: 48.0,
-                    ),
-                    Row(
-                      children: [
-                        ButtonSelect(
-                          title: S.of(context).line,
-                          onPressed: () {
-                            context
-                                .read<Providers>()
-                                .managerScreen(TheCars.ROUTE, context);
-                          },
-                        ),
-                        const SizedBox(
-                          width: 5.0,
-                        ),
-                        ButtonSelect(
-                          title: S.of(context).professions,
-                          onPressed: () {
-                            context.read<Providers>().managerScreen(
-                                ProfessionsScreen.ROUTE, context);
-                          },
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 48.0,
-                    ),
-                    Row(
-                      children: [
-                        ButtonSelect(
-                          title: S.of(context).internal_transfer,
-                          onPressed: () {
-                            context
-                                .read<Providers>()
-                                .managerScreen(SatotaScreen.ROUTE, context);
-                          },
-                        ),
-                      ],
+                    TextButton(
+                      onPressed: () {
+                        if (shared!.getString('lang') == 'ar') {
+                          Navigator.of(context).pop();
+                          return;
+                        }
+                        read.changeLanguage('ar');
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('العربية'),
                     ),
                   ],
                 ),
-              
-            ),
-         
-          ],
+              );
+            },
+          )
+        ],
+      ),
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: getWidth(2),
+          vertical: getheight(2.5),
         ),
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.5),
+              blurRadius: 24,
+              offset: const Offset(0, 10))
+        ]),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: BottomNavigationBar(
+            // backgroundColor: Colors.transparent,
+            selectedItemColor: ColorUsed.second,
+            unselectedItemColor: Colors.black,
+            currentIndex: read.index,
+            onTap: (index) {
+              read.changeSelect(index);
+            },
+            items: [
+              BottomNavigationBarItem(
+                key: navdoctor,
+                icon: Icon(
+                  Icons.medical_information,
+                ),
+                label: S.of(context).doctor,
+              ),
+              BottomNavigationBarItem(
+                key: work,
+                icon: Icon(Icons.work_history),
+                label: S.of(context).professions,
+              ),
+              BottomNavigationBarItem(
+                key: donors,
+                icon: Icon(Icons.bloodtype),
+                label: S.of(context).blood_type,
+              ),
+              BottomNavigationBarItem(
+                key: taxi,
+                icon: Icon(Icons.local_taxi),
+                label: S.of(context).Cars,
+              ),
+              BottomNavigationBarItem(
+                key: stota,
+                icon: Icon(Icons.motorcycle_sharp),
+                label: S.of(context).internal_transfer,
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: context.watch<MainController>().bodys[read.index],
+    );
+  }
+
+  void showTutorial() {
+    tutorialCoachMark.show(context: context);
+  }
+
+  List<TargetFocus> targets = [];
+  void createTutorial() {
+    tutorialCoachMark = TutorialCoachMark(
+      targets: targets,
+      colorShadow: Colors.red,
+      textSkip: S.current.skip,
+      paddingFocus: 10,
+      opacityShadow: 0.5,
+      imageFilter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+      onFinish: () {
+        shared!.setBool('tutorial', true);
+        Logger.logger('message finish ');
+      },
+      onClickTarget: (target) {
+        print('onClickTarget: $target');
+      },
+      onClickTargetWithTapPosition: (target, tapDetails) {
+        Logger.logger("target: $target");
+
+        print("target: $target");
+        print(
+            "clicked at position local: ${tapDetails.localPosition} - global: ${tapDetails.globalPosition}");
+      },
+      onClickOverlay: (target) {
+        print('onClickOverlay: $target');
+      },
+      onSkip: () {
+        print("skip");
+        return true;
+      },
+    );
+  }
+
+  GlobalKey navdoctor = GlobalKey();
+  GlobalKey donors = GlobalKey();
+  GlobalKey work = GlobalKey();
+  GlobalKey taxi = GlobalKey();
+  GlobalKey stota = GlobalKey();
+  GlobalKey lang = GlobalKey();
+
+
+  addItem() {
+    targets.add(
+      TargetFocus(
+        identify: "keyBottomNavigation1",
+        keyTarget: navdoctor,
+        alignSkip: Alignment.topRight,
+        enableOverlayTab: true,
+        contents: [
+          TargetContent(
+            align: ContentAlign.top,
+            builder: (context, controller) {
+              return const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Titulo lorem ipsum",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      ),
+    );
+    targets.add(
+      TargetFocus(
+        identify: "keyBottomNavigation1",
+        keyTarget: work,
+        alignSkip: Alignment.topRight,
+        enableOverlayTab: true,
+        contents: [
+          TargetContent(
+            align: ContentAlign.top,
+            builder: (context, controller) {
+              return const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Titulo lorem ipsum",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      ),
+    );
+    //doctor ,work ,donors ,taxi ,stota
+    targets.add(
+      TargetFocus(
+        identify: "keyBottomNavigation2",
+        keyTarget: donors,
+        alignSkip: Alignment.topRight,
+        enableOverlayTab: true,
+        contents: [
+          TargetContent(
+            align: ContentAlign.top,
+            builder: (context, controller) {
+              return const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Titulo lorem ipsum",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      ),
+    );
+    //doctor ,work ,donors ,taxi ,stota
+    targets.add(
+      TargetFocus(
+        identify: "keyBottomNavigation3",
+        keyTarget: taxi,
+        alignSkip: Alignment.topRight,
+        enableOverlayTab: true,
+        contents: [
+          TargetContent(
+            align: ContentAlign.top,
+            builder: (context, controller) {
+              return const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Titulo lorem ipsum",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      ),
+    );
+    //doctor ,work ,donors ,taxi ,stota
+ 
+    targets.add(
+      TargetFocus(
+        identify: "keyBottomNavigation4",
+        keyTarget: stota,
+        alignSkip: Alignment.topRight,
+        enableOverlayTab: true,
+        contents: [
+          TargetContent(
+            align: ContentAlign.top,
+            builder: (context, controller) {
+              return const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Titulo lorem ipsum",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      ),
+    );
+       targets.add(
+      TargetFocus(
+        identify: "keyBottomNavigation4",
+        keyTarget: lang,
+        alignSkip: Alignment.topRight,
+        enableOverlayTab: true,
+        contents: [
+          TargetContent(
+            align: ContentAlign.bottom,
+            builder: (context, controller) {
+              return const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Titulo lorem ipsum",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
       ),
     );
   }
